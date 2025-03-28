3e64ff014bc830a295ed808290dc1676978ee3cf
Robert Munteanu <rombert@apache.org>
2025-03-28 14:21:34 +0100

    SLING-12723 - Migrate JUnit teleporter to Jakarta JSON

--- src/main/java/org/apache/sling/testing/teleporter/client/TeleporterHttpClient.java
+++ src/main/java/org/apache/sling/testing/teleporter/client/TeleporterHttpClient.java
@@ -34,14 +34,14 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import javax.json.Json;
-import javax.json.JsonArray;
-import javax.json.JsonException;
-import javax.json.JsonObject;
-import javax.json.JsonReader;
-import javax.json.JsonString;
-import javax.json.JsonValue;
-import javax.json.JsonValue.ValueType;
+import jakarta.json.Json;
+import jakarta.json.JsonArray;
+import jakarta.json.JsonException;
+import jakarta.json.JsonObject;
+import jakarta.json.JsonReader;
+import jakarta.json.JsonString;
+import jakarta.json.JsonValue;
+import jakarta.json.JsonValue.ValueType;
 
 import org.apache.commons.io.IOUtils;
 import org.junit.runner.Result;
